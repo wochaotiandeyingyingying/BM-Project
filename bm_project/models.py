49fd742cf8ca41cc2eb294452713866169beb8df
xiaoxiaobochaotiandeyingyingying <475364662@qq.com>
2018-12-24 14:16:20 +0800

    test

--- bm_project/models.py
+++ bm_project/models.py
@@ -19,12 +19,25 @@ class MaterialManager(models.Manager):
         return super(MaterialManager,self).get_queryset().filter()
 
 class Material(models.Model):
-    matobj=MaterialManager()
-    materialid=models.CharField(max_length=30,verbose_name="mpid")
-    prettyformula=models.CharField(max_length=30,verbose_name="化学式")
-    elements=models.CharField(max_length=30,verbose_name="元素")
-    structure=models.CharField(max_length=500,verbose_name="结构")
-    information=models.CharField(max_length=500,verbose_name="信息")
+    matobj = MaterialManager()
+    materialid = models.CharField(max_length=30, verbose_name="mpid")
+    prettyformula = models.CharField(max_length=30, verbose_name="化学式")
+    elements = models.CharField(max_length=30, verbose_name="元素")
+    structure = models.CharField(max_length=500, verbose_name="结构")
+    information = models.CharField(max_length=500, verbose_name="信息")
+    energy = models.CharField(max_length=100, verbose_name="能量")
+    energyperatom = models.CharField(max_length=100)
+    volum = models.CharField(max_length=100)
+    formationenergyperatom = models.CharField(max_length=100)
+    nsites = models.CharField(max_length=100)
+    ishubbard = models.BooleanField()
+    nelements = models.CharField(max_length=100)
+    eabovehull = models.CharField(max_length=100)
+    iscompatible = models.BooleanField()
+    bandgap = models.CharField(max_length=100)
+    density = models.CharField(max_length=100)
+    totalmagnetization = models.CharField(max_length=100)
+    oxidetype = models.CharField(max_length=100)
 
     class Meta:
         db_table="material"
